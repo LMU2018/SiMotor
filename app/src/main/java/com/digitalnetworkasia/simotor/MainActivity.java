26d67c300481f210ccc9b94381ff01e3de3e3bd6
andrealfarysie <alfarysie@gmail.com>
2019-03-01 14:36:15 +0700

    tes commit

--- app/src/main/java/com/digitalnetworkasia/simotor/MainActivity.java
+++ app/src/main/java/com/digitalnetworkasia/simotor/MainActivity.java
@@ -7,10 +7,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText nohp;
     Button button;
+    SignInButton google;
+
+    private static final int RC_SIGN_IN = 234;
+
+    //Tag for the logs optional
+    private static final String TAG = "simplifiedcoding";
+
+    //creating a GoogleSignInClient object
+
+    //And also a Firebase Auth object
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +34,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         nohp = findViewById(R.id.nohp);
         button = findViewById(R.id.button);
+        google = findViewById(R.id.sign_in_button);
         //te
+
+
     }
 
     @Override
@@ -28,15 +47,15 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String mobile =  nohp.getText().toString().trim();
+                String mobile = nohp.getText().toString().trim();
 
-                if (mobile.isEmpty() || mobile.length()<10){
+                if (mobile.isEmpty() || mobile.length() < 10) {
                     nohp.setError("No HP salah");
                     nohp.requestFocus();
                     return;
                 }
-                Intent intent = new Intent(MainActivity.this,AuthActivity.class);
-                intent.putExtra("mobile",mobile);
+                Intent intent = new Intent(MainActivity.this, AuthActivity.class);
+                intent.putExtra("mobile", mobile);
                 startActivity(intent);
             }
         });
